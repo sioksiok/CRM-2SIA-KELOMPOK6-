f60086d96f13d77fc75100039b05e1a22ac147af
adelina <adelina23si@mahasiswa.pcr.ac.id>
2025-05-26 14:59:23 +0700

    Pelanggan done by adelins

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import CustomerManagement from "./pages/CustomerManagement";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+         <Route path="/customer" element={<CustomerManagement />} />
       </Route>
     </Routes>
   );
