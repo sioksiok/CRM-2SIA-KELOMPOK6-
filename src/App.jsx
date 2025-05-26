8b2b5960e400a94437a2b441662d1e62d37e1db1
Putri <alifia23si@mahasiswa.pcr.ac.id>
2025-05-26 14:56:24 +0700

    penjualan sudah oke by putri

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
+import SalesManagement from "./pages/SalesManagement";
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/penjualan" element={<SalesManagement />} />
       </Route>
     </Routes>
   );
